5cac53f6d68b80b90b6efcb01a6fae228673d2df
Mohammed Sayed <mohamadsayed7070@gmail.com>
2023-02-19 19:59:25 +0400

    init

---
+++ PlayWave/Source/PlayerView/WavePlayer.swift
@@ -0,0 +1,20 @@
+//
+//  WavePlayer.swift
+//  PlayWave
+//
+//  Created by Syber Expertise on 19/02/2023.
+//
+
+import UIKit
+
+class WavePlayer: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
