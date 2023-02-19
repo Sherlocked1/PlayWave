25745dccdd2b89cb256b93b65c26b48663a01850
Mohammed Sayed <mohamadsayed7070@gmail.com>
2023-02-19 21:51:35 +0400

    xcframework

---
+++ PlayerWave.xcframework/ios-arm64/PlayWave.framework/Headers/PlayWave.h
@@ -0,0 +1,18 @@
+//
+//  PlayWave.h
+//  PlayWave
+//
+//  Created by Syber Expertise on 19/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PlayWave.
+FOUNDATION_EXPORT double PlayWaveVersionNumber;
+
+//! Project version string for PlayWave.
+FOUNDATION_EXPORT const unsigned char PlayWaveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlayWave/PublicHeader.h>
+
+
